c3768febf29d4245ca0b2cc8843a44328472c4e6
YarKhim <160514711+YarKhim@users.noreply.github.com>
2026-06-21 21:00:02 +0300

    Добавлено отображение задач в Infolist

--- app/Filament/Resources/Tasks/Schemas/TaskInfolist.php
+++ app/Filament/Resources/Tasks/Schemas/TaskInfolist.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Tasks\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class TaskInfolist
@@ -11,24 +12,38 @@ class TaskInfolist
     {
         return $schema
             ->components([
+                Section::make(fn ($livewire) => 'Задача #' . $livewire->record->id)
+                    ->schema([
+                        TextEntry::make('source.source_name')
+                            ->label('Источник')
+                            ->badge()
+                            ->columnStart(1),
+                        TextEntry::make('difficulty_level')
+                            ->badge()
+                            ->label('Уровень сложности')
+                            ->columnStart(1),
+                        Section::make()->schema([
+
+                        ])->columnStart(2)
+                        ->columnSpan(3)
+                ])->columns(4),
                 /*TextEntry::make('id_subject')
                     ->numeric(),*/
-                TextEntry::make('source.source_name'),
+
                 /*TextEntry::make('id_task_source')
                     ->numeric(),*/
-                TextEntry::make('subject.subject_name'),
-                TextEntry::make('task_type')
-                    ->badge(),
-                TextEntry::make('task_number_in_the_kim')
-                    ->numeric(),
-                TextEntry::make('difficulty_level')
-                    ->badge(),
+                #TextEntry::make('subject.subject_name'),
+                /*TextEntry::make('task_type')
+                    ->badge(),*/
+                /*TextEntry::make('task_number_in_the_kim')
+                    ->numeric(),*/
+
                 /*TextEntry::make('created_at')
                     ->dateTime()
                     ->placeholder('-'),
                 TextEntry::make('updated_at')
                     ->dateTime()
                     ->placeholder('-'),*/
-            ]);
+            ])->columns(1);
     }
 }
