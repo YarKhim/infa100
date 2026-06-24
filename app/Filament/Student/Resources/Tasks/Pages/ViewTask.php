692d1148feb2f51bf0077e40e87d41238242976c
YarKhim <160514711+YarKhim@users.noreply.github.com>
2026-06-24 14:41:17 +0300

    Добавлена возможность решать задачи и проверка заданий с кратким ответом

--- app/Filament/Student/Resources/Tasks/Pages/ViewTask.php
+++ app/Filament/Student/Resources/Tasks/Pages/ViewTask.php
@@ -23,15 +23,22 @@ class ViewTask extends ViewRecord
                 ->icon('heroicon-o-pencil')
                 ->color('success')
                 ->action(function (Task $record) {
-                    // Создаём или получаем существующее решение (на всякий случай)
-                    $solution = UserSolution::firstOrCreate(
-                        [
+                    // Создаём или получаем существующее решение
+                    $solution = UserSolution::where('user_id', Auth::id())->where('task_id', $record->id)->first();
+                    if($solution==null) {
+                        $solution = UserSolution::create([
                             'user_id' => Auth::id(),
                             'task_id' => $record->id,
-                            'user_answer'  => '',
-                            'state'  => 'new',
+                            'user_answer' => '',
+                            'state' => 'new',
+                        ]);
+                    }
+                    //dump($solution);
+                    /*$solution = UserSolution::firstOrCreate(
+                        [
+
                         ]
-                    );
+                    );*/
                     // Перенаправляем на страницу редактирования решения
                     return redirect()->to(
                         UserSolutionResource::getUrl('edit', ['record' => $solution])
--- app/Filament/Student/Resources/UserSolutions/Pages/EditUserSolution.php
+++ app/Filament/Student/Resources/UserSolutions/Pages/EditUserSolution.php
@@ -3,9 +3,13 @@
 namespace App\Filament\Student\Resources\UserSolutions\Pages;
 
 use App\Filament\Student\Resources\UserSolutions\UserSolutionResource;
+use App\Models\Task;
+use App\Models\UserSolution;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditUserSolution extends EditRecord
 {
@@ -15,7 +19,34 @@ class EditUserSolution extends EditRecord
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
+            //DeleteAction::make(),
         ];
+    }
+    protected function getSaveFormAction(): \Filament\Actions\Action
+    {
+        return parent::getSaveFormAction()
+            ->label('Сохранить ответ');
+    }
+    protected function beforeSave(): void
+    {
+        /**/
+
+        // Например, проверить права или установить значения
+        $this->data['state'] = 'answer_isnt_given';
+    }
+    protected function afterSave():void
+    {
+        $task_id  = $this->getRecord()->id;
+        $correct_answer =  Task::where('id', $task_id)->first()->answer;
+        $user_answer = $this->getRecord()->user_answer;
+        $solution = UserSolution::where('task_id', $task_id)->where('user_id', Auth::id())->first();
+        if($user_answer==$correct_answer) {
+            $solution->state = 'correct_answer_has_been_given';
+        }
+        else{
+            $solution->state = 'incorrect_answer_given';
+        }
+        $solution->save();
+        //$this->save();
     }
 }
